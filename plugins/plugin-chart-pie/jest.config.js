24366e1d136a19b6b5f53b5d880b6427a3821801
amitmiran137 <amit.miran@nielsen.com>
2021-05-08 15:17:28 +0300

    chore: delete template plugins

--- plugins/plugin-chart-pie/jest.config.js
+++
@@ -1,8 +0,0 @@
-const base = require("../../jest.config.base.js");
-const packagesFile = require('./package.json')
-
-module.exports = {
-    ...base,
-    name: packagesFile.name,
-    setupFilesAfterEnv: ['../../setupJest.js']
-};
